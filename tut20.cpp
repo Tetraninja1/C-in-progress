9cdb9c0bc0b9f297cbb9134d82c3fffebfddfa9d
shreyash <shreyashkatiyaer2020@gmail.com>
2025-08-27 08:03:27 +0530

    cpp in it

--- tut20.cpp
+++ tut20.cpp
@@ -6,9 +6,16 @@ class employee {
         int a,b,c;
     public:
         int d,e;
+        void setdata (int a,int b,int c);
+        void getdata ();
+
 };
 int main() {
-
+    cout<<"the value of a"<<endl;
+    cout<<"the value of b"<<endl;
+    cout<<"the value of c"<<endl;
+    cout<<"the value of d"<<endl;
+    cout<<"the value of e"<<endl;
 
     return 0;
 }
