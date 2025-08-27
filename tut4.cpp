7429c0d846633d88b5125f624a296480316438e8
shreyash <shreyashkatiyaer2020@gmail.com>
2025-08-27 16:49:45 +0530

    lost files recreated once again

--- tut4.cpp
+++ tut4.cpp
@@ -6,8 +6,12 @@ using namespace std;
 int main(){
     int a=13,b=56;
     float c=56.65;
+    char d= 'e';
+    bool e=true;
     cout<<"the value of a is "<<endl<<a<<endl;
     cout<<"the value of b is "<<endl<<b<<endl;
     cout<<"the value of c is "<<endl<<c<<endl;
+    cout<<"the value of d is "<<endl<<d<<endl;
+    cout<<"the value of e is "<<endl<<e<<endl;
     return 0;
 }
