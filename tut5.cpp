70f710194edd24625f8d8e6e2b335e3bc98f550c
shreyash <shreyashkatiyaer2020@gmail.com>
2025-08-27 16:53:44 +0530

    lost files recreated once again

--- tut5.cpp
+++ tut5.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+// program about input and output
 
+int main(){
+    float a,b;
+    cout<<"Enter the numbers a and b"<<endl;
+    cin>>a>>b;
+    cout<<(a*b);
     return 0;
 }
