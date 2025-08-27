c37432c3fac184113c3c76ca0b73dcf2980ceb4e
shreyash <shreyashkatiyaer2020@gmail.com>
2025-08-27 17:13:03 +0530

    lost files recreated once again 2

---
+++ tut6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// operators in cpp
+
+int main(){
+    int a,b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"The value of a + b"<<endl<<(a+b)<<endl;
+    cout<<"The value of a - b"<<endl<<(a-b)<<endl;
+    cout<<"The value of a * b"<<endl<<(a*b)<<endl;
+    cout<<"The value of a / b"<<endl<<(a/b)<<endl;
+    cout<<"The value of a % b"<<endl<<(a%b)<<endl;
+    cout<<"The value of a  b"<<endl<<(a+b)<<endl;
+    cout<<"The value of a++ "<<endl<<(a++)<<endl;
+    cout<<"The value of ++a"<<endl<<(++a)<<endl;
+    cout<<"The value of a--"<<endl<<(a--)<<endl;
+    cout<<"The value of --a"<<endl<<(--a)<<endl;
+    return 0;
+}
